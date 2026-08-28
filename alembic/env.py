9e6f268dbbf669845c66cd896308a3ffdd0271c8
Kame Yoko <kjyu1292@gmail.com>
2026-08-28 16:11:14 +0800

    remove core

--- alembic/env.py
+++ alembic/env.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 import app.environment
-from app.database.core import Base
+from app.database import Base
 
 
 """----------------------------"""
