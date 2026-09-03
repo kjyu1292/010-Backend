3c3fafe4857c7a05f993323e67e2538308e6ebcb
Kame Yoko <kjyu1292@gmail.com>
2026-09-03 13:39:43 +0800

    small fix

--- app/limiter.py
+++ app/limiter.py
@@ -9,8 +9,11 @@ import app.environment
 """----------------------------"""
 limiter_enabled = True if os.getenv("LIMITER_ENABLED") == "true" else False
 
+print(f"LIMITER_ENABLED={os.getenv('LIMITER_ENABLED')}")
+print(f"limiter_enabled={limiter_enabled}")
+
 
 """----------------------------"""
 limiter = Limiter(key_func = get_remote_address, enabled = limiter_enabled)
 
-
+print(f"limiter.enabled     = {limiter.enabled}")
